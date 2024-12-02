6e03f9d3167e16121b17c1d4f808b0bcf335b872
princexoleo <xtleon.xl@gmail.com>
2024-12-02 11:17:03 +0600

    Update the package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="llama_ocr",
+    name="llama_ocr_py",
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
